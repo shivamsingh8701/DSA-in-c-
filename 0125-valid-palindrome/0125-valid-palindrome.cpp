0f6fd489f9d0a441712926fa99038b0176f35f6b
Shivam Singh <work.shivam701@gmail.com>
2026-07-20 03:35:16 -0600

    Time: 0 ms (100%), Space: 9.8 MB (78.14%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
-    bool isalphaNum(char ch ){
-                if((ch>='0' && ch <='9') || (tolower(ch) >= 'a' && tolower(ch)<='z')){
-                    return true ;
-                }
-                return false;
-    }
     bool isPalindrome(string str) {
             int start = 0 ;
             int end = str.length()-1;
             while(start<end){
-               if(!isalphaNum(str[start])){
+               if(!isalnum(str[start])){
                     start++ ; continue;
                }
-               if(!isalphaNum(str[end])){
+            if(!isalnum(str[end])){
                     end-- ; continue;
                }
 
